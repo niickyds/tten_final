b60c72e66443931c2e45eb336f32139f9b184901
adeptexotica <ntsd1158@gmail.com>
2024-09-13 09:13:28 -0500

    feat: search styles / team section button update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "../HomePage/HomePage";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
--- src/components/TeamContent/TeamContent.jsx
+++ src/components/TeamContent/TeamContent.jsx
@@ -5,38 +5,67 @@ function TeamContent() {
   return (
     <div className="landing">
       <div className="landing__team-section">
-        <div className="landing__team-title">My Team</div>
+        <div className="landing__team-title">
+          My Team <br />
+          (search for a pokemon, then add it from recently viewed)
+        </div>
         <div className="landing__team">
           <div className="landing__card">
-            <img className="landing__card-image" src={logo} /> Treeko
-          </div>
-          <div className="landing__card">
-            <img className="landing__card-image" src={logo} /> Treeko
-          </div>
-          <div className="landing__card">
-            <img className="landing__card-image" src={logo} /> Treeko
-          </div>
-          <div className="landing__card">
-            <img className="landing__card-image" src={logo} /> Treeko
-          </div>
-          <div className="landing__card">
+            <div className="card__button-holder">
+              <button className="card__button_add">-</button>
+              <button className="card__button_remove">+</button>
+            </div>
             <img className="landing__card-image" src={logo} />
-            <div>Treeko</div>
+            <div className="landing__card-text">Treeko</div>
           </div>
           <div className="landing__card">
-            <img className="landing__card-image" src={logo} /> Treeko
+            <div className="card__button-holder">
+              <button className="team__add">-</button>
+              <button className="team__remove">+</button>
+            </div>
+            <img className="landing__card-image" src={logo} />
+            <div className="landing__card-text">Treeko</div>
           </div>
           <div className="landing__card">
-            <img className="landing__card-image" src={logo} /> Treeko
+            <div className="card__button-holder">
+              <button className="team__add">-</button>
+              <button className="team__remove">+</button>
+            </div>
+            <img className="landing__card-image" src={logo} />
+            <div className="landing__card-text">Treeko</div>
+          </div>
+          <div className="landing__card">
+            <div className="card__button-holder">
+              <button className="team__add">-</button>
+              <button className="team__remove">+</button>
+            </div>
+            <img className="landing__card-image" src={logo} />
+            <div className="landing__card-text">Treeko</div>
+          </div>
+          <div className="landing__card">
+            <div className="card__button-holder">
+              <button className="team__add">-</button>
+              <button className="team__remove">+</button>
+            </div>
+            <img className="landing__card-image" src={logo} />
+            <div className="landing__card-text">Treeko</div>
+          </div>
+          <div className="landing__card">
+            <div className="card__button-holder">
+              <button className="team__add">-</button>
+              <button className="team__remove">+</button>
+            </div>
+            <img className="landing__card-image" src={logo} />
+            <div className="landing__card-text">Treeko</div>
           </div>
         </div>
       </div>
 
-      <div className="landing__pokedex_holder">
-        <button className="pokedex_button">pokedex</button>
+      <div className="landing__pokedex">
+        <button className="landing__button">pokedex</button>
       </div>
-      <div>
-        <button className="pokedex_button">random pokemon button</button>
+      <div className="landing__random">
+        <button className="landing__button">random pokemon button</button>
       </div>
     </div>
   );
