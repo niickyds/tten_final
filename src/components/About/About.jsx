8082c4de46f84fdfca9e8f6383cc6ae46013cc72
adeptexotica <ntsd1158@gmail.com>
2024-10-25 17:42:24 -0500

    minor fixes / about update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,11 +8,23 @@ function AboutPage() {
         <div className="about__holder">
           <h3 className="about__title">About Me</h3>
           <p className="about__content">
-            Filler words, also known as vocal disfluencies or hesitations, are
-            words or sounds that people use to indicate they are thinking but
-            have not finished speaking. They are often used excessively, but can
-            be removed without changing the meaning of the sentence.
+            Hello, I'm Nick, a dedicated Full Stack Engineer that studied under
+            TripleTen. My journey studying under them has been incredibly
+            rewarding, allowing me to take ownership of projects and experience
+            rapid professional growth through continuous learning. Each project
+            presents a unique opportunity for me to expand my technical
+            repertoire and push the boundaries of innovation. Let's connect and
+            explore opportunities to collaborate on exciting ventures together!
           </p>
+          {/* <br /> */}
+          Link to my github:{" "}
+          <a
+            href="https://github.com/niickyds?tab=repositories"
+            className="about__link"
+            target="_blank"
+          >
+            here
+          </a>
         </div>
       </div>
       <Footer />
